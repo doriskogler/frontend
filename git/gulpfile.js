69d65410a255e46c05aeafcb15d394743311e3a8
doris <S1310456019@students.fh-hagenberg.at>
2015-06-21 20:35:41 +0200

    commit changes

--- git/gulpfile.js
+++ git/gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require("gulp");
 
-var jshint = require("gulp-jshint");
+//var jshint = require("gulp-jshint");
 
 var changed = require("gulp-changed");
 var imagemin = require("gulp-imagemin");
+
+var minifyHTML = require("gulp-minify-html");
 
 gulp.task("jshint", function() {
     gulp.src("./src/scripts/*.js")
@@ -12,11 +14,22 @@ gulp.task("jshint", function() {
 });
 
 gulp.task("imagemin", function() {
-    var imgSrc = "./src/images/**/*",
-        imgDst = "./build/images";
+    var imgSrc = "gulpdemo-master/src/images/**/*",
+        imgDst = "gulpdemo-master/build/images";
 
     gulp.src(imgSrc)
         .pipe(changed(imgDst))
         .pipe(imagemin())
         .pipe(gulp.dest(imgDst));
+});
+
+gulp.task("htmlpage", function() {
+   var htmlSrc ="gulpdemo-master/src/*.html",
+       htmlDst ="gulpdemo-master/build";
+
+    gulp.src(htmlSrc)
+        .pipe(changed(htmlDst))
+        .pipe(minifyHTML())
+        .pipe(gulp.dest(htmlDst));
+
 });
